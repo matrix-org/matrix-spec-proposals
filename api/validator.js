caa84c1dc172fa930f37b8f4a841dd2fc8bda3a9
Kegan Dougal <kegan@matrix.org>
2015-05-29 12:15:24 +0100

    Add Swagger 2 docs for content repo. Add Swagger 2 CLI validator.

---
+++ api/validator.js
@@ -0,0 +1,34 @@
+"use strict";
+var nopt = require("nopt");
+var parser = require("swagger-parser");
+var path = require("path");
+
+var opts = nopt({
+    "help": Boolean,
+    "schema": path
+}, {
+    "h": "--help",
+    "s": "--schema"
+});
+
+if (opts.help) {
+    console.log(
+        "Use swagger-parser to validate against Swagger 2.0\n"+
+        "Usage:\n"+
+        "  node validator.js -s <schema_file>"
+    );
+    process.exit(0);
+}
+if (!opts.schema) {
+    console.error("No [s]chema specified.");
+    process.exit(1);
+}
+
+parser.parse(opts.schema, function(err, api, metadata) {
+    if (!err) {
+        console.log("%s is valid.", opts.schema);
+        return;
+    }
+    console.log(metadata);
+    console.error(err);
+});
