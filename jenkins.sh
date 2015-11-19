757b0bcd12269b0a9905efee7520e61abc81ec7e
Daniel Wagner-Hall <daniel@matrix.org>
2015-11-19 16:08:37 -0500

    Try to build continuserv and speculator

--- jenkins.sh
+++ jenkins.sh
@@ -7,3 +7,8 @@ set -ex
 (cd scripts && ./gendoc.py -v)
 (cd api && npm install && node validator.js -s "client-server/v1" && node validator.js -s "client-server/v2_alpha")
 (cd event-schemas/ && ./check.sh)
+
+if which go >/dev/null 2>/dev/null; then
+  (cd scripts/continuserv && go build)
+  (cd scripts/speculator && go build)
+fi
