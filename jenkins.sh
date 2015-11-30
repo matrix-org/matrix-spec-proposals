6c66bfc75589392b61d67e1a37c3f11b66a5cf7e
Daniel Wagner-Hall <daniel@matrix.org>
2015-11-30 11:22:57 +0000

    Flatten out v1 and v2_alpha directories
    As a side effect, I got rid of all of the horrible symlinks and just put
    in all of the proper relative paths. Because the horrible symlinks were
    horrible.

--- jenkins.sh
+++ jenkins.sh
@@ -5,7 +5,7 @@ set -ex
 (cd event-schemas/ && ./check_examples.py)
 (cd api && ./check_examples.py)
 (cd scripts && ./gendoc.py -v)
-(cd api && npm install && node validator.js -s "client-server/v1" && node validator.js -s "client-server/v2_alpha")
+(cd api && npm install && node validator.js -s "client-server")
 (cd event-schemas/ && ./check.sh)
 
 : ${GOPATH:=${WORKSPACE}/.gopath}
